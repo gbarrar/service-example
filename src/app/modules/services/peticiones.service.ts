80a988c7e25350eb4b0ed5b9a11e4884bc906d32
Gonzalo Barra <gonzalobarra@MacBook-Air-de-Gonzalo.local>
2018-04-11 00:48:46 -0300

    testing

--- src/app/modules/services/peticiones.service.ts
+++ src/app/modules/services/peticiones.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class PeticionesService{
---
+++ src/public_api.ts
@@ -0,0 +1,3 @@
+export * from './app/modules/peticiones/peticiones.module';
+export * from './app/modules/peticiones/peticiones.component';
+export * from './app/modules/services/peticiones.service';
